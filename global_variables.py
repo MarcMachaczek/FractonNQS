4f4b963a3d414ed60f46bcaac3f3f42be29cf6db
Marc <matzcy@live.de>
2022-09-19 12:56:50 +0200

    added repo structure

---
+++ global_variables.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_PATH = os.path.join(PROJECT_PATH,'settings.yaml')
+BREAST_MNIST_PATH = os.path.join(PROJECT_PATH,'data')
