607042e9cf4f1cf752fd4a4efd76e4c7d0fbe01d
Marc <matzcy@live.de>
2022-12-16 20:10:27 +0100

    added mcmc sampling, training for all 4 versions of RBM (real-complex, symmetric), tuned hyperparameters for mcmc, cleaned code

--- prototype_toric2D.py
+++ prototype_toric2D.py
@@ -7,8 +7,18 @@ import netket as nk
 
 import geneqs
 
+# %% setting hyper-parameters
+n_iter = 300
+n_chains = 512  # total number of MCMC chains, should be large when runnning on GPU  ~O(1000)
+n_samples = n_chains*32  # each chain will have length 32
+n_discard_per_chain = 20  # should be small for when using many chains, default is 10% of n_samples (too big)
+# n_sweeps will default to n_sites, every n_sweeps (updates) a sample will be generated
+
+preconditioner = nk.optimizer.SR(diag_shift=0.01)
+
+
 # %% Define graph/lattice and hilbert space
-L = 3  # size should be at least 3, else there are problems with pbc and indexing
+L = 5  # size should be at least 3, else there are problems with pbc and indexing
 shape = jnp.array([L, L])
 square_graph = nk.graph.Square(length=L, pbc=True)
 hilbert = nk.hilbert.Spin(s=1/2, N=square_graph.n_edges)
@@ -37,54 +47,59 @@ link_perms = nk.utils.HashableArray(link_perms.astype(int))
 
 # start with exact sampling to avoid MCMC issues
 #%% train regular RBM
-sampler = nk.sampler.ExactSampler(hilbert)
+# sampler = nk.sampler.ExactSampler(hilbert)
+sampler = nk.sampler.MetropolisLocal(hilbert, n_chains=n_chains)
 RBM = nk.models.RBM(alpha=2)
 sgd = nk.optimizer.Sgd(learning_rate=0.1)
-vqs = nk.vqs.MCState(sampler, RBM, n_samples=1024)
+vqs = nk.vqs.MCState(sampler, RBM, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
 
-gs = nk.driver.VMC(toric, sgd, variational_state=vqs, preconditioner=nk.optimizer.SR(diag_shift=0.01))
+gs = nk.driver.VMC(toric, sgd, variational_state=vqs, preconditioner=preconditioner)
 
 log = nk.logging.RuntimeLog()
-gs.run(n_iter=300, out=log)
+gs.run(n_iter=n_iter, out=log)
 data_rbm = log.data
 
 # %% train symmetric RBM
-sampler = nk.sampler.ExactSampler(hilbert)
+# sampler = nk.sampler.ExactSampler(hilbert)
+sampler = nk.sampler.MetropolisLocal(hilbert, n_chains=n_chains)
 RBM_symm = nk.models.RBMSymm(symmetries=link_perms, alpha=2)
 sgd_symm = nk.optimizer.Sgd(learning_rate=0.05)
-vqs_symm = nk.vqs.MCState(sampler, RBM_symm, n_samples=1024)
+# n_samples is divided by n_chains for the length of any MCMC chain
+vqs_symm = nk.vqs.MCState(sampler, RBM_symm, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
 
 vqs_symm.init_parameters()
-gs_symm = nk.driver.VMC(toric, sgd_symm, variational_state=vqs_symm, preconditioner=nk.optimizer.SR(diag_shift=0.01))
+gs_symm = nk.driver.VMC(toric, sgd_symm, variational_state=vqs_symm, preconditioner=preconditioner)
 
 log = nk.logging.RuntimeLog()
-gs_symm.run(n_iter=300, out=log)
+gs_symm.run(n_iter=n_iter, out=log)
 data_symm = log.data
 
 # %% train complex RBM
-sampler = nk.sampler.ExactSampler(hilbert)
+# sampler = nk.sampler.ExactSampler(hilbert)
+sampler = nk.sampler.MetropolisLocal(hilbert, n_chains=n_chains)
 RBM_mp = nk.models.RBMModPhase(alpha=2)
-sgd_mp = nk.optimizer.Sgd(learning_rate=0.1)
-vqs_mp = nk.vqs.MCState(sampler, RBM_mp, n_samples=1024)
+sgd_mp = nk.optimizer.Sgd(learning_rate=0.13)
+vqs_mp = nk.vqs.MCState(sampler, RBM_mp, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
 
 vqs_mp.init_parameters()
-gs_mp = nk.driver.VMC(toric, sgd_mp, variational_state=vqs_mp, preconditioner=nk.optimizer.SR(diag_shift=0.01))
+gs_mp = nk.driver.VMC(toric, sgd_mp, variational_state=vqs_mp, preconditioner=preconditioner)
 
 log = nk.logging.RuntimeLog()
-gs_mp.run(n_iter=300, out=log)
+gs_mp.run(n_iter=n_iter, out=log)
 data_mp = log.data
 
 # %% train symmetric complex RBM
-sampler = nk.sampler.ExactSampler(hilbert)
+# sampler = nk.sampler.ExactSampler(hilbert)
+sampler = nk.sampler.MetropolisLocal(hilbert, n_chains=n_chains)
 RBM_symm_mp = geneqs.models.RBMModPhaseSymm(symmetries=link_perms, alpha=2)
 sgd_symm_mp = nk.optimizer.Sgd(learning_rate=0.08)
-vqs_symm_mp = nk.vqs.MCState(sampler, RBM_symm_mp, n_samples=1024*4)
+vqs_symm_mp = nk.vqs.MCState(sampler, RBM_symm_mp, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
 
 vqs_symm_mp.init_parameters()
-gs_symm_mp = nk.driver.VMC(toric, sgd_symm_mp, variational_state=vqs_symm_mp, preconditioner=nk.optimizer.SR(diag_shift=0.01))
+gs_symm_mp = nk.driver.VMC(toric, sgd_symm_mp, variational_state=vqs_symm_mp, preconditioner=preconditioner)
 
 log = nk.logging.RuntimeLog()
-gs_symm_mp.run(n_iter=300, out=log)
+gs_symm_mp.run(n_iter=n_iter, out=log)
 data_symm_mp = log.data
 
 # %%
