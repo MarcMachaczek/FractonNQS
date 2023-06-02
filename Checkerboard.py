energy derivative in plotting, fixed cb plotting bugs (like hilbert size), abs magnetization in all directions, implemented new hist method into all scripts, changed to finite differences function

Checkerboard.py
@@ -27,8 +27,8 @@ cube_graph = nk.graph.Hypercube(length=L, n_dim=3, pbc=True)
 hilbert = nk.hilbert.Spin(s=1 / 2, N=cube_graph.n_nodes)
 
 # define some observables
-magnetization = 1 / hilbert.size * sum([nk.operator.spin.sigmaz(hilbert, i) for i in range(hilbert.size)])
-abs_magnetization = geneqs.operators.observables.AbsMagnetization(hilbert)
+magnetization = 1 / hilbert.size * sum([nk.operator.spin.sigmax(hilbert, i) for i in range(hilbert.size)])
+abs_magnetization = geneqs.operators.observables.AbsXMagnetization(hilbert)
 
 # visualize the graph
 fig = plt.figure(figsize=(10, 10), dpi=300)
@@ -114,7 +114,7 @@ transition_steps = int(n_iter / 3)
 lr_schedule = optax.linear_schedule(lr_init, lr_end, transition_steps, transition_begin)
 
 # define fields for which to trian the NQS and get observables
-direction = np.array([0., 0., 0.8]).reshape(-1, 1)
+direction = np.array([0.8, 0., 0.]).reshape(-1, 1)
 field_strengths = (np.linspace(0, 1, 20) * direction).T
 # field_strengths = np.vstack((field_strengths, np.array([[0.31, 0, 0],
 #                                                         [0.32, 0, 0],
@@ -122,10 +122,10 @@ field_strengths = (np.linspace(0, 1, 20) * direction).T
 #                                                         [0.34, 0, 0],
 #                                                         [0.35, 0, 0]])))
 # for which fields indices histograms are created
-hist_fields = np.array([[0., 0, 0.3],
-                        [0., 0, 0.4],
-                        [0., 0, 0.42],
-                        [0., 0, 0.6]])
+hist_fields = np.array([[0.3, 0, 0.],
+                        [0.4, 0, 0.],
+                        [0.45, 0, 0.],
+                        [0.6, 0, 0.]])
 # make sure hist fields are contained in field_strengths and sort final field array
 field_strengths = np.unique(np.round(np.vstack((field_strengths, hist_fields)), 3), axis=0)
 
@@ -182,13 +182,13 @@ for h in tqdm(field_strengths, "external_field"):
     if np.any((h == hist_fields).all(axis=1)):
         variational_gs.n_samples = n_samples
         # calculate histograms, CAREFUL: if run with mpi, local_estimators produces rank-dependent output!
-        e_locs = np.asarray((variational_gs.local_estimators(checkerboard)).real, dtype=np.float64)
+        e_locs = np.asarray((variational_gs.local_estimators(checkerboard)), dtype=np.float64)
         observables.add_hist("energy", h, np.histogram(e_locs / hilbert.size, n_bins, density=False))
 
-        mag_locs = np.asarray((variational_gs.local_estimators(magnetization)).real, dtype=np.float64)
+        mag_locs = np.asarray((variational_gs.local_estimators(magnetization)), dtype=np.float64)
         observables.add_hist("mag", h, np.histogram(mag_locs, n_bins, density=False))
 
-        abs_mag_locs = np.asarray((variational_gs.local_estimators(abs_magnetization)).real, dtype=np.float64)
+        abs_mag_locs = np.asarray((variational_gs.local_estimators(abs_magnetization)), dtype=np.float64)
         observables.add_hist("abs_mag", h, np.histogram(abs_mag_locs, n_bins, density=False))
 
     # plot and save training data

Checkerboard_MPI.py
@@ -22,6 +22,7 @@ from netket.utils import HashableArray
 
 import geneqs
 from geneqs.utils.training import loop_gs
+from geneqs.utils.eval_obs import get_locests_mixed
 from global_variables import RESULTS_PATH
 
 from matplotlib import pyplot as plt
@@ -43,7 +44,7 @@ hilbert = nk.hilbert.Spin(s=1 / 2, N=cube_graph.n_nodes)
 
 # define some observables
 magnetization = 1 / hilbert.size * sum([nk.operator.spin.sigmax(hilbert, i) for i in range(hilbert.size)])
-abs_magnetization = geneqs.operators.observables.AbsMagnetization(hilbert)
+abs_magnetization = geneqs.operators.observables.AbsZMagnetization(hilbert)
 
 perms = geneqs.utils.indexing.get_translations_cubical3d(shape, shift=2)
 perms = nk.utils.HashableArray(perms.astype(int))
@@ -124,16 +125,16 @@ lr_schedule = optax.linear_schedule(lr_init, lr_end, transition_steps, transitio
 
 # define fields for which to trian the NQS and get observables
 direction = np.array([0.8, 0., 0.]).reshape(-1, 1)
-field_strengths = (np.linspace(0, 1, 16) * direction).T
-# field_strengths = np.vstack((field_strengths, np.array([[0.31, 0, 0],
-#                                                         [0.32, 0, 0],
-#                                                         [0.33, 0, 0],
-#                                                         [0.34, 0, 0],
-#                                                         [0.35, 0, 0]])))
+field_strengths = (np.linspace(0, 1, 9) * direction).T
+field_strengths = np.vstack((field_strengths, np.array([[0.42, 0, 0],
+                                                        [0.43, 0, 0],
+                                                        [0.44, 0, 0],
+                                                        [0.45, 0, 0],
+                                                        [0.46, 0, 0]])))
 # for which fields indices histograms are created
 hist_fields = np.array([[0.3, 0, 0.],
-                        [0.4, 0, 0.],
-                        [0.42, 0, 0.],
+                        [0.43, 0, 0.],
+                        [0.44, 0, 0.],
                         [0.6, 0, 0.]])
 # make sure hist fields are contained in field_strengths and sort final field array
 field_strengths = np.unique(np.round(np.vstack((field_strengths, hist_fields)), 3), axis=0)
@@ -189,12 +190,13 @@ for h in tqdm(field_strengths, "external_field"):
 
     if np.any((h == hist_fields).all(axis=1)):
         variational_gs.n_samples = n_samples
+        random_key, init_state_key = jax.random.split(random_key)
         energy_locests = comm.gather(
-            np.asarray(variational_gs.local_estimators(checkerboard).real, dtype=np.float64), root=0)
+            np.asarray(get_locests_mixed(init_state_key, variational_gs, checkerboard), dtype=np.float64), root=0)
         mag_locests = comm.gather(
-            np.asarray(variational_gs.local_estimators(magnetization).real, dtype=np.float64), root=0)
+            np.asarray(get_locests_mixed(init_state_key, variational_gs, magnetization), dtype=np.float64), root=0)
         abs_mag_locests = comm.gather(
-            np.asarray(variational_gs.local_estimators(abs_magnetization).real, dtype=np.float64), root=0)
+            np.asarray(get_locests_mixed(init_state_key, variational_gs, abs_magnetization), dtype=np.float64), root=0)
 
     # plot and save training data, save observables
     if rank == 0:

Toric2D_h.py
@@ -29,7 +29,7 @@ hilbert = nk.hilbert.Spin(s=1 / 2, N=square_graph.n_edges)
 
 # define some observables
 magnetization = 1 / hilbert.size * sum([nk.operator.spin.sigmaz(hilbert, i) for i in range(hilbert.size)])
-abs_magnetization = geneqs.operators.observables.AbsMagnetization(hilbert)
+abs_magnetization = geneqs.operators.observables.AbsZMagnetization(hilbert)
 wilsonob = geneqs.operators.observables.get_netket_wilsonob(hilbert, shape)
 
 positions = jnp.array([[i, j] for i in range(shape[0]) for j in range(shape[1])])
@@ -193,19 +193,19 @@ for h in tqdm(field_strengths, "external_field"):
         variational_gs.n_samples = n_samples
         random_key, init_state_key = jax.random.split(random_key)
         # calculate histograms, CAREFUL: if run with mpi, local_estimators produces rank-dependent output!
-        e_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, toric).real,
+        e_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, toric),
                             dtype=np.float64)
         observables.add_hist("energy", h, np.histogram(e_locs / hilbert.size, n_bins, density=False))
 
-        mag_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, magnetization).real,
+        mag_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, magnetization),
                               dtype=np.float64)
         observables.add_hist("mag", h, np.histogram(mag_locs, n_bins, density=False))
 
-        abs_mag_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, abs_magnetization).real,
+        abs_mag_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, abs_magnetization),
                                   dtype=np.float64)
         observables.add_hist("abs_mag", h, np.histogram(abs_mag_locs, n_bins, density=False))
 
-        A_B_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, A_B).real,
+        A_B_locs = np.asarray(get_locests_mixed(init_state_key, variational_gs, A_B),
                               dtype=np.float64)
         observables.add_hist("A_B", h, np.histogram(A_B_locs, n_bins, density=False))
 

geneqs/operators/observables.py
@@ -5,14 +5,13 @@ import netket as nk
 from typing import Tuple
 from functools import partial
 
-import geneqs
 from geneqs.utils.indexing import edge_to_index
 
 
 # %%
-class AbsMagnetization(nk.operator.DiscreteOperator):
+class AbsZMagnetization(nk.operator.DiscreteOperator):
     """
-    Operator corresponing to the absolute magnetization of a state. For regular magnetization simply use NetKet loca
+    Operator corresponing to the absolute magnetization of a state. For regular magnetization simply use NetKet local
     operators.
     """
 
@@ -39,10 +38,102 @@ class AbsMagnetization(nk.operator.DiscreteOperator):
             connected states or "kets" eta, corresponding matrix elements mels
 
         """
-        return abs_magnetization_conns_and_mels(sigma)
+        return abs_zmagnetization_conns_and_mels(sigma)
 
     def get_conn_flattened(self, x, sections):
-        eta, mels = abs_magnetization_conns_and_mels(x)
+        eta, mels = abs_zmagnetization_conns_and_mels(x)
+
+        n_primes = eta.shape[1]  # number of connected states, same for all possible configurations x
+        n_visible = x.shape[1]
+        eta = eta.reshape(-1, n_visible)  # flatten last dimension
+        mels = mels.flatten()
+
+        # must manipulate sections in place
+        for i in range(len(sections)):
+            sections[i] = (i + 1) * n_primes
+
+        return eta, mels
+
+
+class AbsXMagnetization(nk.operator.DiscreteOperator):
+    """
+    Operator corresponing to the absolute magnetization of a state. For regular magnetization simply use NetKet local
+    operators.
+    """
+
+    def __init__(self, hilbert: nk.hilbert.DiscreteHilbert):
+        super().__init__(hilbert)
+
+    @property
+    def dtype(self):
+        return float
+
+    @property
+    def is_hermitian(self):
+        return True
+
+    def get_conn_padded(self, sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
+        """
+        See Netket repo for details. This method is called by <get_local_kernel_arguments> for discrete operators.
+        Instead of implementing the flattened version, we can directly overwrite the padded method, which is more
+        efficient.
+        Args:
+            sigma: Input states or "bra", acting from the left.
+
+        Returns:
+            connected states or "kets" eta, corresponding matrix elements mels
+
+        """
+        return abs_xmagnetization_conns_and_mels(sigma)
+
+    def get_conn_flattened(self, x, sections):
+        eta, mels = abs_xmagnetization_conns_and_mels(x)
+
+        n_primes = eta.shape[1]  # number of connected states, same for all possible configurations x
+        n_visible = x.shape[1]
+        eta = eta.reshape(-1, n_visible)  # flatten last dimension
+        mels = mels.flatten()
+
+        # must manipulate sections in place
+        for i in range(len(sections)):
+            sections[i] = (i + 1) * n_primes
+
+        return eta, mels
+
+
+class AbsYMagnetization(nk.operator.DiscreteOperator):
+    """
+    Operator corresponing to the absolute magnetization of a state. For regular magnetization simply use NetKet loca
+    operators.
+    """
+
+    def __init__(self, hilbert: nk.hilbert.DiscreteHilbert):
+        super().__init__(hilbert)
+
+    @property
+    def dtype(self):
+        return complex
+
+    @property
+    def is_hermitian(self):
+        return True
+
+    def get_conn_padded(self, sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
+        """
+        See Netket repo for details. This method is called by <get_local_kernel_arguments> for discrete operators.
+        Instead of implementing the flattened version, we can directly overwrite the padded method, which is more
+        efficient.
+        Args:
+            sigma: Input states or "bra", acting from the left.
+
+        Returns:
+            connected states or "kets" eta, corresponding matrix elements mels
+
+        """
+        return abs_ymagnetization_conns_and_mels(sigma)
+
+    def get_conn_flattened(self, x, sections):
+        eta, mels = abs_ymagnetization_conns_and_mels(x)
 
         n_primes = eta.shape[1]  # number of connected states, same for all possible configurations x
         n_visible = x.shape[1]
@@ -57,20 +148,23 @@ class AbsMagnetization(nk.operator.DiscreteOperator):
 
 
 @nk.vqs.get_local_kernel.dispatch
-def get_local_kernel(vstate: nk.vqs.MCState, op: AbsMagnetization):
-    return e_loc
+def get_local_kernel(vstate: nk.vqs.MCState, op: AbsZMagnetization):
+    return abs_e_loc
 
 
-@nk.vqs.get_local_kernel_arguments.dispatch
-def get_local_kernel_arguments(vstate: nk.vqs.MCState, op: AbsMagnetization):
-    sigma = vstate.samples
-    extra_args = abs_magnetization_conns_and_mels(sigma.reshape(-1, vstate.hilbert.size))
-    return sigma, extra_args
+@nk.vqs.get_local_kernel.dispatch
+def get_local_kernel(vstate: nk.vqs.MCState, op: AbsXMagnetization):
+    return abs_e_loc
+
+
+@nk.vqs.get_local_kernel.dispatch
+def get_local_kernel(vstate: nk.vqs.MCState, op: AbsYMagnetization):
+    return abs_e_loc
 
 
 @jax.jit
 @partial(jax.vmap, in_axes=(0,))
-def abs_magnetization_conns_and_mels(sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
+def abs_zmagnetization_conns_and_mels(sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
     """
     For a given input spin configuration sigma, calculates all connected states eta and the corresponding non-zero
     matrix elements mels. See netket for further details.
@@ -88,6 +182,64 @@ def abs_magnetization_conns_and_mels(sigma: jax.Array) -> Tuple[jax.Array, jax.A
     # connected matrix elements
     n_sites = sigma.shape[-1]
     mels = jnp.absolute(jnp.sum(sigma, axis=-1, keepdims=True)) / n_sites
+
+    return eta, mels
+
+
+@jax.jit
+@partial(jax.vmap, in_axes=(0,))
+def abs_xmagnetization_conns_and_mels(sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
+    """
+    For a given input spin configuration sigma, calculates all connected states eta and the corresponding non-zero
+    matrix elements mels. See netket for further details.
+    Args:
+        sigma: Input state or "bra", acting from the left.
+
+    Returns:
+        connected states or "kets" eta, corresponding matrix elements mels
+
+    """
+    n_sites = sigma.shape[-1]
+
+    @jax.vmap
+    def flip(x, idx):
+        return x.at[idx].set(-x.at[idx].get())
+
+    # connected states is sigma itself
+    eta = jnp.tile(sigma, (n_sites, 1))
+    eta = eta.at[:].set(flip(eta.at[:].get(), jnp.arange(n_sites)))
+
+    # connected matrix elements
+    mels = jnp.ones(n_sites) / n_sites
+
+    return eta, mels
+
+
+@jax.jit
+@partial(jax.vmap, in_axes=(0,))
+def abs_ymagnetization_conns_and_mels(sigma: jax.Array) -> Tuple[jax.Array, jax.Array]:
+    """
+    For a given input spin configuration sigma, calculates all connected states eta and the corresponding non-zero
+    matrix elements mels. See netket for further details.
+    Args:
+        sigma: Input state or "bra", acting from the left.
+
+    Returns:
+        connected states or "kets" eta, corresponding matrix elements mels
+
+    """
+    n_sites = sigma.shape[-1]
+
+    @jax.vmap
+    def flip(x, idx):
+        return x.at[idx].set(-x.at[idx].get())
+
+    # connected states is sigma itself
+    eta = jnp.tile(sigma, (n_sites, 1))
+    eta = eta.at[:].set(flip(eta.at[:].get(), jnp.arange(n_sites)))
+
+    # connected matrix elements
+    mels = 1j * sigma / n_sites
 
     return eta, mels
 
@@ -112,6 +264,30 @@ def e_loc(logpsi, pars, sigma, extra_args):
     @partial(jax.vmap, in_axes=(0, 0, 0))
     def _loc_vals(sigma, eta, mels):
         return jnp.sum(mels * jnp.exp(logpsi(pars, eta) - logpsi(pars, sigma)), axis=-1)
+
+    return _loc_vals(sigma, eta, mels)
+
+
+def abs_e_loc(logpsi, pars, sigma, extra_args):
+    """
+    Calculates the absolute local estimate of the operator.
+    Args:
+        logpsi: Wavefunction, taking as input pars and some state, returning the log amplitude.
+        pars: Parameters for logpsi model.
+        sigma: Samples from which to calculate the estimate.
+        extra_args: Connected states, non-zero matrix elements.
+
+    Returns:
+        local estimates: | sum_{eta} <sigma|Operator|eta> * psi(eta)/psi(sigma) | over different sigmas
+
+    """
+    eta, mels = extra_args
+    assert sigma.ndim == 2, f"sigma dimensions should be (Nsamples, Nsites), but has dimensions {sigma.shape}"
+    assert eta.ndim == 3, f"eta dimensions should be (Nsamples, Nconnected, Nsites), but has dimensions {eta.shape}"
+
+    @partial(jax.vmap, in_axes=(0, 0, 0))
+    def _loc_vals(sigma, eta, mels):
+        return jnp.abs(jnp.sum(mels * jnp.exp(logpsi(pars, eta) - logpsi(pars, sigma)), axis=-1))
 
     return _loc_vals(sigma, eta, mels)
 

geneqs/utils/eval_obs.py
@@ -118,25 +118,26 @@ class ObservableCollector:
 
 
 # %%
-def susc_from_mag(magnetizations: Union[jax.Array, np.ndarray], fields: Union[jax.Array, np.ndarray]):
+def derivative_fd(observable: Union[jax.Array, np.ndarray], fields: Union[jax.Array, np.ndarray]):
     """
-    Calculate the magnetic susceptibility from observed magnetization values via finite differences.
-    This function also returns the external field values for which the susceptibilites were evaluated.
+    Calculate the derivative of observable values via finite differences.
+    This function also returns the external "field" values for which the differences were evaluated.
     Args:
-        magnetizations: Magnetization values for different external fields.
+        observable: Observable values for different external fields.
         fields: Array of shape (n_fields, 3) containing for each magnetization the external field in x, y, z direction.
+        Actually, this also works for arbitrary second dimension.
 
     Returns:
-        susceptibility array (n_fields), susceptibility fields (n_fields, 3)
+        derivative array (n_fields), susceptibility fields (n_fields, 3)
 
     """
     
-    assert len(magnetizations) == len(fields), "length of magnetizations and fields must be the same"
+    assert len(observable) == len(fields)
 
-    sus = (magnetizations[1:] - magnetizations[:-1]) / np.linalg.norm(fields[1:] - fields[:-1], axis=1)
-    sus_fields = (fields[1:] + fields[:-1]) / 2
+    derivative = (observable[1:] - observable[:-1]) / np.linalg.norm(fields[1:] - fields[:-1], axis=1)
+    derivative_fields = (fields[1:] + fields[:-1]) / 2
 
-    return sus, sus_fields
+    return derivative, derivative_fields
 
 
 # %%
@@ -201,4 +202,4 @@ def get_locests_mixed(rng_key,
     cold_locests = locests_from_startstate(vqs, operator, cold_batch, n_discard)
     random_locests = locests_from_startstate(vqs, operator, random_batch, n_discard)
     locests = jnp.concatenate((converged_locests, cold_locests, random_locests), axis=0).flatten()
-    return locests
+    return locests.real

plot_cb_observables.py
@@ -7,9 +7,10 @@ from global_variables import RESULTS_PATH
 
 cmap = matplotlib.colormaps["Set1"]
 f_dict = {0: "x", 1: "y", 2: "z"}
+
 # %%
 L = 4
-hilbert_size = 2 * L ** 2
+hilbert_size = L ** 3
 eval_model = "CheckerCRBM"
 save_dir = f"{RESULTS_PATH}/checkerboard"
 obs_list = []
@@ -19,10 +20,11 @@ obs_list.append(
 
 direction = obs_list[0][-1, :3]
 field_direction = 0  # 0=x, 1=y, 2=z
+
 # %% magnetizations comparison
-# obs columns: "hx, hy, hz, energy, energy_var, mag, mag_var, abs_mag, abs_mag_var, wilson, wilson_var, exact_energy"
-fig = plt.figure(dpi=300, figsize=(22, 30))
-plot_mag = fig.add_subplot(321)
+# obs columns: "hx, hy, hz, energy, energy_var, mag, mag_var, abs_mag, abs_mag_var, (exact_energy)"
+fig = plt.figure(dpi=300, figsize=(30, 22))
+plot_mag = fig.add_subplot(231)
 
 for i, obs in enumerate(obs_list):
     for ob in obs:
@@ -37,13 +39,14 @@ plot_mag.set_title(
 plot_mag.legend()
 
 # %% absolute magnetization
-plot_abs_mag = fig.add_subplot(322)
+plot_abs_mag = fig.add_subplot(232)
 
 for i, obs in enumerate(obs_list):
     for ob in obs:
         plot_abs_mag.errorbar(ob[field_direction], ob[7], yerr=ob[8], marker="o", markersize=2, color=cmap(i))
 
-    plot_abs_mag.plot(obs[:, field_direction], obs[:, 7], marker="o", markersize=2, color=cmap(i), label=f"h={obs[-1][:3]}")
+    plot_abs_mag.plot(obs[:, field_direction], obs[:, 7], marker="o", markersize=2, color=cmap(i),
+                      label=f"h={obs[-1][:3]}")
 
 plot_abs_mag.set_xlabel(f"external field h{f_dict[field_direction]}")
 plot_abs_mag.set_ylabel("absolute magnetization")
@@ -52,11 +55,12 @@ plot_abs_mag.set_title(
 plot_abs_mag.legend()
 
 # %% susceptibility
-plot_sus = fig.add_subplot(323)
+plot_sus = fig.add_subplot(233)
 
 for i, obs in enumerate(obs_list):
-    sus, sus_fields = geneqs.utils.eval_obs.susc_from_mag(magnetizations=obs[:, 5], fields=obs[:, :3])
-    plot_sus.plot(sus_fields[:, field_direction], sus, marker="o", markersize=2, color=cmap(i), label=f"h={obs[-1][:3]}")
+    sus, sus_fields = geneqs.utils.eval_obs.derivative_fd(observable=obs[:, 5], fields=obs[:, :3])
+    plot_sus.plot(sus_fields[:, field_direction], sus, marker="o", markersize=2, color=cmap(i),
+                  label=f"h={obs[-1][:3]}")
 
 plot_sus.set_xlabel(f"external field h{f_dict[field_direction]}")
 plot_sus.set_ylabel("susceptibility of magnetization")
@@ -65,11 +69,10 @@ plot_sus.set_title(
 plot_sus.legend()
 
 # %% energy per site
-plot_energy = fig.add_subplot(325)
+plot_energy = fig.add_subplot(234)
 
 for i, obs in enumerate(obs_list):
-    n_sites = L ** 2 * 2
-    plot_energy.plot(obs[:, field_direction], obs[:, 3] / n_sites, marker="o", markersize=2, color=cmap(i),
+    plot_energy.plot(obs[:, field_direction], obs[:, 3] / hilbert_size, marker="o", markersize=2, color=cmap(i),
                      label=f"h={obs[-1][:3]}")
 
 plot_energy.set_xlabel(f"external field h{f_dict[field_direction]}")
@@ -79,8 +82,22 @@ plot_energy.set_title(
 
 plot_energy.legend()
 
+# %% energy derivative dE/dh
+plot_dEdh = fig.add_subplot(235)
+
+for i, obs in enumerate(obs_list):
+    dEdh, fd_fields = geneqs.utils.eval_obs.derivative_fd(observable=obs[:, 3], fields=obs[:, :3])
+    plot_dEdh.plot(fd_fields[:, field_direction], dEdh, marker="o", markersize=2, color=cmap(i),
+                   label=f"h={obs[-1][:3]}")
+
+plot_dEdh.set_xlabel(f"external field h{f_dict[field_direction]}")
+plot_dEdh.set_ylabel("dE / dh")
+plot_dEdh.set_title(
+    f"energy derivative vs external field in {direction}-direction for Checkerboard of shape=[{L},{L},{L}]")
+plot_dEdh.legend()
+
 # %% specific heat / variance of the energy
-plot_spheat = fig.add_subplot(326)
+plot_spheat = fig.add_subplot(236)
 
 for i, obs in enumerate(obs_list):
     plot_spheat.plot(obs[:, field_direction], np.abs(obs[:, 4]), marker="o", markersize=2, color=cmap(i),
@@ -93,7 +110,7 @@ plot_spheat.set_title(
 
 plot_spheat.legend()
 plt.show()
-fig.savefig(f"{RESULTS_PATH}/checkerboard/obs_comparison_L[{L} {L} {L}]_cRBM.pdf")
+fig.savefig(f"{save_dir}/obs_comparison_L[{L} {L} {L}]_cRBM.pdf")
 
 # %%%%%%%%%%%%%%% HISTOGRAMS %%%%%%%%%%%%%%% #
 # shape is (n_hist_fields, 3), where 3 = field_value + hist_values + bin_edges
@@ -111,7 +128,7 @@ abs_mag_hist = fig.add_subplot(223)
 obs = obs_list[0]
 for hist in energy_histograms:
     field, hist_values, bin_edges = np.round(hist[0], 3), hist[1], hist[2]
-    e_mean = obs[np.argwhere((np.round(obs[:, :3], 3)==field).all(axis=1)), 3].item()
+    e_mean = obs[np.argwhere(np.all(np.round(obs[:, :3], 3) == field, axis=1)), 3].item()
     edges = (bin_edges[1:] + bin_edges[:-1]) / 2 - e_mean / hilbert_size
     hist_values_rescaled = hist_values / np.sum(hist_values)
     energy_hist.plot(edges, hist_values_rescaled, label=f"h={tuple([round(h, 3) for h in field])}")

plot_tc_observables.py
@@ -43,7 +43,8 @@ for i, obs in enumerate(obs_list):
     for ob in obs:
         plot_abs_mag.errorbar(ob[field_direction], ob[7], yerr=ob[8], marker="o", markersize=2, color=cmap(i))
 
-    plot_abs_mag.plot(obs[:, field_direction], obs[:, 7], marker="o", markersize=2, color=cmap(i), label=f"h={obs[-1][:3]}")
+    plot_abs_mag.plot(obs[:, field_direction], obs[:, 7], marker="o", markersize=2, color=cmap(i),
+                      label=f"h={obs[-1][:3]}")
 
 plot_abs_mag.set_xlabel(f"external field h{f_dict[field_direction]}")
 plot_abs_mag.set_ylabel("absolute magnetization")
@@ -55,8 +56,9 @@ plot_abs_mag.legend()
 plot_sus = fig.add_subplot(323)
 
 for i, obs in enumerate(obs_list):
-    sus, sus_fields = geneqs.utils.eval_obs.susc_from_mag(magnetizations=obs[:, 5], fields=obs[:, :3])
-    plot_sus.plot(sus_fields[:, field_direction], sus, marker="o", markersize=2, color=cmap(i), label=f"h={obs[-1][:3]}")
+    sus, sus_fields = geneqs.utils.eval_obs.derivative_fd(observable=obs[:, 5], fields=obs[:, :3])
+    plot_sus.plot(sus_fields[:, field_direction], sus, marker="o", markersize=2, color=cmap(i),
+                  label=f"h={obs[-1][:3]}")
 
 plot_sus.set_xlabel(f"external field h{f_dict[field_direction]}")
 plot_sus.set_ylabel("susceptibility of magnetization")
@@ -71,7 +73,8 @@ for i, obs in enumerate(obs_list):
     for ob in obs:
         plot_wilson.errorbar(ob[field_direction], ob[9], yerr=ob[10], marker="o", markersize=2, color=cmap(i))
 
-    plot_wilson.plot(obs[:, field_direction], obs[:, 9], marker="o", markersize=2, color=cmap(i), label=f"h={obs[-1][:3]}")
+    plot_wilson.plot(obs[:, field_direction], obs[:, 9], marker="o", markersize=2, color=cmap(i),
+                     label=f"h={obs[-1][:3]}")
 
 plot_wilson.set_xlabel(f"external field h{f_dict[field_direction]}")
 plot_wilson.set_ylabel("wilson loop < prod A_s * prod B_p >")
@@ -128,7 +131,7 @@ A_B_hist = fig.add_subplot(224)
 obs = obs_list[0]
 for hist in energy_histograms:
     field, hist_values, bin_edges = np.round(hist[0], 3), hist[1], hist[2]
-    e_mean = obs[np.argwhere((np.round(obs[:, :3], 3)==field).all(axis=1)), 3].item()
+    e_mean = obs[np.argwhere(np.all(np.round(obs[:, :3], 3) == field, axis=1)), 3].item()
     edges = (bin_edges[1:] + bin_edges[:-1]) / 2 - e_mean / hilbert_size
     hist_values_rescaled = hist_values / np.sum(hist_values)
     energy_hist.plot(edges, hist_values_rescaled, label=f"h={tuple([round(h, 3) for h in field])}")

toric_vs_ed.py
@@ -28,7 +28,7 @@ hilbert = nk.hilbert.Spin(s=1 / 2, N=square_graph.n_edges)
 
 # define some observables
 magnetization = 1 / hilbert.size * sum([nk.operator.spin.sigmaz(hilbert, i) for i in range(hilbert.size)])
-abs_magnetization = geneqs.operators.observables.AbsMagnetization(hilbert)
+abs_magnetization = geneqs.operators.observables.AbsZMagnetization(hilbert)
 wilsonob = geneqs.operators.observables.get_netket_wilsonob(hilbert, shape)
 
 positions = jnp.array([[i, j] for i in range(shape[0]) for j in range(shape[1])])